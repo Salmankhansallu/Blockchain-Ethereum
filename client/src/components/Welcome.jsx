d8bda79aafaaa7e5516678445e9b52d01b3648a6
salman khan <78694695+Salmankhansallu@users.noreply.github.com>
2023-03-23 11:39:56 +0530

    Update keyword

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -21,11 +21,11 @@ const Welcome=()=>{
   const {connectWallet,currentAccount,formData,sendTransaction,handleChange,isLoading}=useContext(TransactionContext);
   
     const handleSubmit=(e)=>{
-      const { addressTo, amount, keyword, message } = formData;
+      const { addressTo, amount, message } = formData;
 
       e.preventDefault();
   
-      if (!addressTo || !amount || !keyword || !message) return;
+      if (!addressTo || !amount  || !message) return;
   
       sendTransaction();
     }
@@ -94,7 +94,7 @@ const Welcome=()=>{
             <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
             <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
             <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
-            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+
             <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
             <div className="h-[1px] w-full bg-gray-400 my-2" />
             
